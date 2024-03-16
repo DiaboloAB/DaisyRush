4ee5c34f227258f50e4aee1efc55023826815227
Diabolo <alexis.boitel@epitech.eu>
2024-03-16 10:44:33 +0100

    test(): empty player to test entt

---
+++ src/player/Player.cpp
@@ -0,0 +1,8 @@
+/*
+** EPITECH PROJECT, 2024
+** DaisyRush
+** File description:
+** Player
+*/
+
+#include "Player.hpp"
---
+++ src/player/Player.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2024
+** DaisyRush
+** File description:
+** Player
+*/
+
+#ifndef PLAYER_HPP_
+#define PLAYER_HPP_
+
+#include "../../lib/entt/entt.hpp"
+
+class Player {
+
+};
+
+#endif /* !PLAYER_HPP_ */
