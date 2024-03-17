272110c0ce5a93f1b1b2e236028edc952b6b15ad
Jean-Yanis Jeffroy <jean-yanis.jeffroy@epitech.eu>
2024-03-17 18:42:30 +0100

    feat(Scoreboard)

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,20 @@
 #include "Core.hpp"
+#include "scoreboard/Scoreboard.hpp"
 
 int main() {
 
-    entt::registry registry;
-    Core core(registry);
-    core.run();
+    Scoreboard scoreboard;
+    scoreboard.updateScores();
+    std::cout << "Scores:" << std::endl;
+
+    for (auto &el : scoreboard.getScores()) {
+        std::cout << std::get<0>(el) << " " << std::get<1>(el) << std::endl;
+    }
+
+
+    // entt::registry registry;
+    // Core core(registry);
+    // core.run();
 
     return 0;
 }
---
+++ src/scoreboard/Scoreboard.hpp
@@ -0,0 +1,51 @@
+/*
+** EPITECH PROJECT, 2024
+** DaisyRush
+** File description:
+** Scoreboard
+*/
+
+#ifndef SCOREBOARD_HPP_
+#define SCOREBOARD_HPP_
+
+#include <string>
+#include <tuple>
+#include <vector>
+#include <sstream>
+
+#include <curlpp/cURLpp.hpp>
+#include <curlpp/Options.hpp>
+
+#include <nlohmann/json.hpp>
+
+class Scoreboard {
+    public:
+        Scoreboard() {};
+        ~Scoreboard() {};
+
+        std::vector<std::tuple<std::string, int>> getScores() const { return _scores; }
+
+        void updateScores() {
+            curlpp::Cleanup myCleanup;
+
+            std::ostringstream os;
+            os << curlpp::options::Url(std::string("https://daisyrush.jam.jeffroy.eu"));
+
+            std::string json = os.str();
+
+            nlohmann::json j = nlohmann::json::parse(json);
+            std::vector<std::tuple<std::string, int>> scores;
+            for (auto &el : j) {
+                scores.push_back(std::make_tuple(el["name"], el["timestamp"]));
+                std::cout << el["name"] << " " << el["timestamp"] << std::endl;
+            }
+            setScores(scores);
+        }
+
+    protected:
+    private:
+        void setScores(std::vector<std::tuple<std::string, int>> scores) { _scores = scores; }
+        std::vector<std::tuple<std::string, int>> _scores;
+};
+
+#endif /* !SCOREBOARD_HPP_ */
